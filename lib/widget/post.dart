6be36ff44febc9585194a72c9cfc74d00cf3a621
Gülsüm Sevim <glsmsvm.12@gmail.com>
2022-11-28 13:52:20 +0300

    second

--- lib/widget/post.dart
+++ lib/widget/post.dart
@@ -6,7 +6,7 @@ class PostPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 1,
+        itemCount: 3,
         itemBuilder: (context, index) {
           return Column(
             children: [
@@ -20,13 +20,72 @@ class PostPage extends StatelessWidget {
                       image:
                           DecorationImage(image: AssetImage('assets/a.png'))),
                 ),
-                title: Text('Can'),
-                subtitle: Text('bla bla'),
+                title: Text('Canan'),
+                subtitle: Text('İstanbul'),
                 trailing: Icon(Icons.more_vert),
               ),
               Container(
-                height: 400,
-              )
+                height: 300,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.contain,
+                    image: AssetImage('assets/indir.jpeg'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.favorite_border,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.chat_bubble_outline_outlined),
+                        ),
+                        Icon(Icons.share),
+                      ],
+                    ),
+                    const Icon(Icons.bookmark)
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text('Liked by '),
+                    Text(
+                      'Burak Yılmaz ',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text('and '),
+                    Text(
+                      'others',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text('Liked by '),
+                    Text(
+                      'bla bla',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
             ],
           );
         });
